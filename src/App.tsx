90d2079a15373a8652de8c3614abbd6845f325d2
MaccKOT <51321216+MaccKOT@users.noreply.github.com>
2021-05-12 21:41:47 +0400

    Fetch from local json working

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,55 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { Box } from '@fower/react';
+import { styled } from '@fower/styled';
+
+interface Pokemon {
+  id: number;
+  name: {
+    english: string;
+    japanese: string;
+  };
+  type: string[];
+  base: Record<string, number>;
+}
+
+const Input = styled('input');
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [filter, setFilter] = useState('');
+  const [allPokemon, setAllPokemon] = useState<Pokemon[]>([]);
+
+  useEffect(() => {
+    fetch('./pokemon.json')
+      .then((resp) => resp.json())
+      .then((pokemon: Pokemon[]) => setAllPokemon(pokemon));
+  }, []);
+
+  const lcFilter = filter.toLowerCase();
+  const pokemon = allPokemon
+    .filter(({ name: { english } }) => english.toLowerCase().includes(lcFilter))
+    .slice(0, 10);
 
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <p>Hello Vite + React!</p>
-        <p>
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-      </header>
-    </div>
+    <Box p-10 maxW-1200 m-auto>
+      <Input
+        p-5
+        text4XL
+        border-1
+        rounded
+        borderGray500
+        w='100%'
+        value={filter}
+        onChange={(evt) => setFilter(evt.target.value)}
+      />
+      <Box>
+        {pokemon.map((pokemon) => (
+          <Box key={pokemon.id} text3XL>
+            {pokemon.name.english}
+          </Box>
+        ))}
+      </Box>
+    </Box>
   );
 }
 
